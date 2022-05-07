13498810f0f0a94970dae84172feee0f07545260
hikornienko <hi.kornienko@gmail.com>
2022-05-07 19:29:03 +0300

    create moon component

--- src/views/components/templates/home/index.tsx
+++ src/views/components/templates/home/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-import { Header } from '../../organisms/Header';
-import { Title } from '../../organisms/Title';
+import { Header } from '../../organisms/header';
+import { Title } from '../../organisms/title';
+import { Moon } from '../../atoms/moon';
 
 export const Home: React.FC = () => {
     return (
         <main>
             <Header />
+            <Moon />
             <Title />
         </main>
     );
